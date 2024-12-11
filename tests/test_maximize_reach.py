21e81b911283628c2175b1ddb44b214c40fed1c0
Tomasz Chmiel <tomasz.chmiel2@edu.uekat.pl>
2024-12-11 12:57:09 +0100

    Change test

--- tests/test_maximize_reach.py
+++ tests/test_maximize_reach.py
@@ -5,8 +5,8 @@ def test_calculate_total_reach_bfs(large_sample_graph):
     reach_count, reach_set = calculate_total_reach_bfs(large_sample_graph, 1, 2)
 
     # Assert
-    assert reach_count == 8
-    assert reach_set == {2, 3, 4, 5, 6, 7, 8, 10}
+    assert reach_count == 7
+    assert reach_set == {2, 3, 4, 5, 6, 8, 10}
 
 def test_find_best_tag_bfs(large_sample_graph):
     # Act
